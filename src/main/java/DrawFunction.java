aeab3580579c81acb1a522a4b8213ee4cf0dbe93
scorpionorange <scorpionorange@126.com>
2017-09-04 23:01:52 +0800

    update at 2017.9.05;

--- src/main/java/DrawFunction.java
+++ src/main/java/DrawFunction.java
@@ -33,4 +33,51 @@ public class DrawFunction extends JFrame {
     public DrawFunction(){
         add(new NewPanel());
     }
+
+    public class Coordinate2D{
+        int x, y;
+        public Coordinate2D(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getPixelPointX(){
+            return x0 + x;
+        }
+        public int getPixelPointY(){
+            return y0 - y;
+        }
+    }
+    class NewPanel extends JPanel{
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            G = g;
+            setOrigin(W / 2, H / 2);
+            // in the following, draw what you want draw!
+            for(int i = -W / 2; i <= W / 2; i++){
+                draw(i, work(i));
+            }
+        }
+    }
+    int work(int x){
+        return (int)(F(x / timesx) * timesy);
+    }
+
+    public void draw(int x, int y){
+        int X = new Coordinate2D(x, y).getPixelPointX();
+        int Y = new Coordinate2D(x, y).getPixelPointY();
+        drawLine(X, Y, X, Y);
+    }
+
+    public void drawRec(int x1, int y1, int x2, int y2){
+        int dx = x1 < x2 ? 1 : -1;
+        int dy = y1 < y2 ? 1 : -1;
+        for(int i = x1; i != x2 + dx; i += dx){
+            for(int j = y1; j != y2 + dy; j += dy){
+                draw(i, j);
+            }
+        }
+    }
+
+
 }
